2f95212fab3689391cb835303bca230a388f1d04
pdanh0312@gmail.com <anh.pham@tuni.fi>
2022-07-10 18:31:42 +0300

    feat: triggering effect when crashing/finishing

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float delayDuration = 1.5f;
+    [SerializeField] ParticleSystem crashEffect;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground"){
-            SceneManager.LoadScene(0);
+            crashEffect.Play();
+            Invoke("ReloadScene", delayDuration);
         }
+        
     }
+
+    void ReloadScene(){
+        SceneManager.LoadScene(0);
+    }
+    
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float delayDuration = 1.5f;
+    [SerializeField] ParticleSystem finishEffect;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player"){
-            SceneManager.LoadScene(0);
+            finishEffect.Play();
+            Invoke("ReloadScene", delayDuration);
         }
         
+    }
 
+    void ReloadScene(){
+        SceneManager.LoadScene(0);
     }
 }
