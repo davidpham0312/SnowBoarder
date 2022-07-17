d6376b27cd62704e82c4f2eed38ef3a272988e10
pdanh0312@gmail.com <anh.pham@tuni.fi>
2022-07-17 16:22:53 +0300

    feat: audio for crashing & winning

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,9 +7,11 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float delayDuration = 1.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground"){
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", delayDuration);
         }
         
